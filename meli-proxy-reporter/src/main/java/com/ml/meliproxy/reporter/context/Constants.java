c6b0b6950ef03fa163d34fded3c89eca9016a7eb
Diego Delgado <diego.delgado@dlocal.com>
2018-02-01 23:35:45 -0300

    init meli proxy reporter

---
+++ meli-proxy-reporter/src/main/java/com/ml/meliproxy/reporter/context/Constants.java
@@ -0,0 +1,8 @@
+package com.ml.meliproxy.reporter.context;
+
+public interface Constants {
+	static final String APP_NAME = "meli-proxy-reporter";
+	static final String DEFAULT_CONTEXT_PATH = "/meli-proxy-reporter";
+	static final String BASE_PACKAGE = "com.ml.meliproxy.reporter";
+	static final String CONTROLLERS_PACKAGE = BASE_PACKAGE + ".controller";
+}
